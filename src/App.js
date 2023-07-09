996cc90208b9112cb106e2e37e9f206c9ce399b1
changho <leecho87@naver.com>
2023-07-09 14:18:50 +0900

    h2 추가

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import React from 'react'
 function App() {
   return (
     <>
-      <h1>h1 텍스트를 추가합니다.</h1>
+      <h1>h1 태그를 추가합니다.</h1>
+      <h2>h2 태그를 추가합니다.</h2>
       <p>p 태그를 추가합니다.</p>
     </>
   )
